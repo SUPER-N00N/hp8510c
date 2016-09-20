376d50c7609f1d8961ddedd4ce685448410c98d0
SUPER N00N <snaske@77k.eu>
2016-09-20 16:25:08 +0200

    v2 added

--- vna2touchstone.py
+++ vna2touchstone.py
@@ -10,6 +10,10 @@ def main():
 	suff = sys.argv[2]
 	start = sys.argv[1]
 	stop = sys.argv[3]
+	touchstonev2 = True;
+	if len(sys.argv) > 4:
+		if sys.argv[5].upper() == "V1":
+			touchstonev2 = False;
 	vna = HP8510C('PROLOGIX::/dev/ttyUSB0::GPIB::16')
 	vna.cmd("++spoll;");
 	vna.cmd("++loc;");
@@ -54,13 +58,24 @@ def main():
 		c = complex(float(c_1[0]), float(c_1[1]));
 		s22.append(c)
 	diff = (float(stop) - float(start)) / 801.0
-	print "!2-port S-parameter file"
-	print "# " + suff + "S RI R 50.0"
-	print "!freq RelS11 ImS11 RelS12 ImS12 RelS21 ImS21 RelS22 ImS22"
+	if touchstonev2 == True :
+		print "!2-port S-parameter file"
+		print "[Version] 2.0"
+		print "# " + suff + " S RI R 50.0"
+		print "!freq RelS11 ImS11 RelS12 ImS12 RelS21 ImS21 RelS22 ImS22"
+		print "[Number of Ports] 2"
+		print "[Number of Frequencies] 801"
+		i = 0
+		for i in range(len(s11)):
+			print "% f" % (float(start) + diff * i) + " " + "% f" % s11[i].real + " " + "% f" % s11[i].imag + " " +  "% f" % s12[i].real + " " + "% f" % s12[i].imag + " " +  "% f" % s21[i].real + " " + "% f" % s21[i].imag + " " +  "% f" % s22[i].real + " " + "% f" % s22[i].imag
+	else:
+		print "!2-port S-parameter file"
+		print "# " + suff + " S RI R 50.0"
+		print "!freq RelS11 ImS11 RelS12 ImS12 RelS21 ImS21 RelS22 ImS22"
 	
-	i = 0
-	for i in range(len(s11)):
-		print "% .4f" % (float(start) + diff * i) + " " + "% .4f" % s11[i].real + " " + "% .4f" % s11[i].imag + "  " +  "% .4f" % s12[i].real + " " + "% .4f" % s12[i].imag + " " +  "% .4f" % s21[i].real + " " + "% .4f" % s21[i].imag + " " +  "% .4f" % s22[i].real + " " + "% .4f" % s22[i].imag
+		i = 0
+		for i in range(len(s11)):
+			print "% .4f" % (float(start) + diff * i) + " " + "% .4f" % s11[i].real + " " + "% .4f" % s11[i].imag + " " +  "% .4f" % s12[i].real + " " + "% .4f" % s12[i].imag + " " +  "% .4f" % s21[i].real + " " + "% .4f" % s21[i].imag + " " +  "% .4f" % s22[i].real + " " + "% .4f" % s22[i].imag
 	
 if __name__ == "__main__":
     main()
