d93e8e9b2bc9fba61e561f337a14b8ed994cd9de
SUPER N00N <snaske@77k.eu>
2016-09-23 23:47:42 +0200

    foop

--- vna2touchstone.py
+++ vna2touchstone.py
@@ -7,6 +7,7 @@ from hp8510c import *
 
 def main():
 
+	numoffreqs = 801
 	suff = sys.argv[2]
 	start = sys.argv[1]
 	stop = sys.argv[3]
@@ -14,57 +15,68 @@ def main():
 	if len(sys.argv) > 4:
 		if sys.argv[5].upper() == "V1":
 			touchstonev2 = False;
+	if len(sys.argv) > 5:
+		numoffreqs = int(sys.argv[6])
 	vna = HP8510C('PROLOGIX::/dev/ttyUSB0::GPIB::16')
 	vna.cmd("++spoll;");
 	vna.cmd("++loc;");
 	time.sleep(1);
-	vna.write("POIN801; WAIT; WAIT;" + "STAR " + start + suff + ";" + "STOP " + stop + suff + ";" + "WAIT; WAIT");
-	vna.write("S11;FORM4;WAIT;WAIT;WAIT;WAIT;");
-	time.sleep(4)
-	res = vna.cmd("OUTPDATA;");
 	s11 = []
 	s12 = []
 	s21 = []
 	s22 = []
-	idx = 0
-	for c_0_s in res:
-		c_1 = c_0_s.split(',')
-		c = complex(float(c_1[0]), float(c_1[1]));
-		idx += 1
-		s11.append(c)
+	fac = numoffreqs / 801
+	offset = 0.0
+	tick = 0.0001
+	step = (float(stop) - float(start))/fac
+	step_start = float(start)
+	for k in range(fac):
+		step_stop = step_start + step;
+		vna.write("POIN801; WAIT; WAIT;" +  "STAR " + str(step_start) + suff + ";" +"STOP " + str(step_stop) + suff + ";" + "WAIT; WAIT");
+		vna.write("S11;FORM4;WAIT;WAIT;WAIT;WAIT;");
+		time.sleep(4)
+		res = vna.cmd("OUTPDATA;");
+		idx = 0
+		for c_0_s in res:
+			c_1 = c_0_s.split(',')
+			c = complex(float(c_1[0]), float(c_1[1]));
+			idx += 1
+			s11.append(c)
 
-	time.sleep(1);
-	vna.write("S12;WAIT;");
-	time.sleep(1)
-	res = vna.cmd("OUTPDATA;");
-	for c_0_s in res:
-		c_1 = c_0_s.split(',')
-		c = complex(float(c_1[0]), float(c_1[1]));
-		s12.append(c)
-	time.sleep(1);
-	vna.write("S21;WAIT;");
-	time.sleep(1)
-	res = vna.cmd("OUTPDATA;");
-	for c_0_s in res:
-		c_1 = c_0_s.split(',')
-		c = complex(float(c_1[0]), float(c_1[1]));
-		s21.append(c)
-	time.sleep(1);
-	vna.write("S22;WAIT;");
-	time.sleep(1)
-	res = vna.cmd("OUTPDATA;");
-	for c_0_s in res:
-		c_1 = c_0_s.split(',')
-		c = complex(float(c_1[0]), float(c_1[1]));
-		s22.append(c)
-	diff = (float(stop) - float(start)) / 801.0
+		time.sleep(1);
+		vna.write("S12;WAIT;");
+		time.sleep(1)
+		res = vna.cmd("OUTPDATA;");
+		for c_0_s in res:
+			c_1 = c_0_s.split(',')
+			c = complex(float(c_1[0]), float(c_1[1]));
+			s12.append(c)
+		time.sleep(1);
+		vna.write("S21;WAIT;");
+		time.sleep(1)
+		res = vna.cmd("OUTPDATA;");
+		for c_0_s in res:
+			c_1 = c_0_s.split(',')
+			c = complex(float(c_1[0]), float(c_1[1]));
+			s21.append(c)
+		time.sleep(1);
+		vna.write("S22;WAIT;");
+		time.sleep(1)
+		res = vna.cmd("OUTPDATA;");
+		for c_0_s in res:
+			c_1 = c_0_s.split(',')
+			c = complex(float(c_1[0]), float(c_1[1]));
+			s22.append(c)
+		step_start = step_stop
+		vna.write("STAR 0.1 GHz;" + "STOP 8.1 GHz;" + "WAIT; WAIT");
+	diff = (float(stop) - float(start)) / numoffreqs
 	if touchstonev2 == True :
 		print "!2-port S-parameter file"
 		print "[Version] 2.0"
 		print "# " + suff + " S RI R 50.0"
 		print "!freq RelS11 ImS11 RelS12 ImS12 RelS21 ImS21 RelS22 ImS22"
 		print "[Number of Ports] 2"
-		print "[Number of Frequencies] 801"
+		print "[Number of Frequencies] " + str(numoffreqs)
 		i = 0
 		for i in range(len(s11)):
 			print "% f" % (float(start) + diff * i) + " " + "% f" % s11[i].real + " " + "% f" % s11[i].imag + " " +  "% f" % s12[i].real + " " + "% f" % s12[i].imag + " " +  "% f" % s21[i].real + " " + "% f" % s21[i].imag + " " +  "% f" % s22[i].real + " " + "% f" % s22[i].imag
